a14deeb45272d69c71ae5b74c97a27658c129987
baesaa0304 <baesaa0304@naver.com>
2024-05-07 16:14:21 +0900

    백준 1158문제

--- algorithms_Java01/src/algorithms_Java01_2/baekjoon1158.java
+++ algorithms_Java01/src/algorithms_Java01_2/baekjoon1158.java
@@ -7,20 +7,23 @@ public class baekjoon1158 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Queue<Integer>que = new LinkedList<Integer>();
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         
-        for (int i = 0; i < N; i++) {
-        	que.offer(i);
+        Queue<Integer>que = new LinkedList<>();
+        
+        for (int i = 1; i < N + 1; i++) {
+        	que.add(i);
         }
         StringBuilder sb = new StringBuilder();
         sb.append("<");
-        while(que.size()!= 1) {
-        	for(int i =0; i < M - 1; i++) {
-            	que.offer(que.poll());
-            	System.out.println("que poll : " + que.offer(que.poll()));
+        
+        while(que.size() !=  1) {
+        	for(int i = 0; i < M - 1; i++) {
+            	que.add(que.poll());
+            	System.out.println(que);
             } 
         	sb.append(que.poll()).append(", ");
         } 
