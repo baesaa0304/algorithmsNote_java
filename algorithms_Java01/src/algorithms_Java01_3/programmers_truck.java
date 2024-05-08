41d26133b110e8222c0d1c0f3bdcaacad78ba168
baesaa0304 <baesaa0304@naver.com>
2024-05-08 17:34:02 +0900

    프로그래머스 다리를 건너는 트럭 완료

--- algorithms_Java01/src/algorithms_Java01_3/programmers_truck.java
+++ algorithms_Java01/src/algorithms_Java01_3/programmers_truck.java
@@ -23,36 +23,45 @@ public class programmers_truck {
 		}
 		
 		// 시작
-		int time = 0; // 시간 초
-		int bride_weight = 0; //다리 위의 무게
 		
-		Queue<Integer> bridge_list = new LinkedList<>(); // 건너야 하는 다리
-		 for (int i = 0; i < truck_weights.length; i++) {
-			 while (true) {
-				 for(int j =0; j < bridge_length; j++) {
-					 if (weight > bride_weight) {
-						 bridge_list.offer(truck_weights[i]);
-						 bride_weight += truck_weights[i];
-						 System.out.println("1. 추가 다리무게 : " + bride_weight);
-						 System.out.println("2. 추가 다리 : " + bridge_list);
-						 time++;
-					 }	 
-					else if (weight <= bride_weight) {
-						bridge_list.poll();
-						System.out.println("3. 빠지고 난 후 다리 : " + bridge_list);
-						time++;
-						System.out.println("4. poll된 시간 :" + time);
-					 }
-					 
-					
-				 }
-				 if (bridge_list.isEmpty()) {
-					 break;
-				 }
-				 
-			 } 
-		 }
-		 System.out.println(time + 1);
+		int answer = 0;
+        int bridge_weight=0;
+        Queue<Integer> bridge_list  = new LinkedList<Integer>();
+        
+        for(int i = 0; i < truck_weights.length; i++){
+        while(true){
+            //다리에 트럭 없는 경우
+            if(bridge_list.isEmpty()){
+                bridge_list.offer(truck_weights[i]);
+                bridge_weight+=truck_weights[i];
+                answer++;
+                break;
+            }
+            //다리가 꽉 찬 경우
+            else if(bridge_list.size()==bridge_length){
+                bridge_weight-=bridge_list.poll();
+            }
+            else{
+                //다리에 트럭을 실을 수 있을 경우
+                if(bridge_weight+truck_weights[i]<=weight){
+                    bridge_list.offer(truck_weights[i]);
+                    bridge_weight+=truck_weights[i];
+                    answer++;
+                    break;
+                }
+                else{
+                    //다리에 트럭 무게가 초과인 경우
+                    //가상으로 0을 채워줌
+                    bridge_list.offer(0);
+                    answer++;
+                }
+                }
+            }
+        }
+        
+        //마지막 트럭이 빠져나오는 시간
+        answer += bridge_length;
+		System.out.println(answer);
 		
 	}
 
