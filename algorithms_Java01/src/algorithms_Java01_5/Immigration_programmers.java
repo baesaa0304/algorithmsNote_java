e863d796ebf79e3dc60db59a162381127d0eaaf3
baesaa0304 <baesaa0304@naver.com>
2024-05-10 22:02:53 +0900

    최종 답안 입국시간

--- algorithms_Java01/src/algorithms_Java01_5/Immigration_programmers.java
+++ algorithms_Java01/src/algorithms_Java01_5/Immigration_programmers.java
@@ -5,23 +5,29 @@ import java.util.*;
 public class Immigration_programmers {
 	public long solution(int n, int[] times) {
 		long answer = 0;
-	    long min_time = times[0];
-	    long max_time =  (long)times[times.length-1]*(long)n;
-	        while(min_time <= max_time) {
-				long mid = (min_time + max_time) / 2;
-	            long people = 0; // 입국 심사 끝난 사람	
-				for(int time : times) {
-					people += mid / time;
-				}
-				if (people >= n) {
-					answer = mid;
-					max_time = mid - 1;
-				}
-				else {
-					min_time = mid + 1;
-				}
+    long min_time = 0;
+    long max_time =  (long)times[times.length-1]*(long)n;
+        while(min_time <= max_time) {
+			long mid = (min_time + max_time) / 2;
+            long people = 0; // 입국 심사 끝난 사람	
+			for(int time : times) {
+				people += mid / time;
 			}
-	        return answer;
-	    }
-	
+			if (people >= n) {
+				answer = mid;
+				max_time = mid - 1;
+			}
+			else {
+				min_time = mid + 1;
+			}
+		}
+        return answer;
+    }
+	public static void main(String[] args) {
+		Immigration_programmers solution = new Immigration_programmers();
+        int n = 6; // 입국 심사를 기다리는 사람의 수
+        int[] times = {7, 10}; // 각 심사관이 한 명을 심사하는데 걸리는 시간
+        long result = solution.solution(n, times);
+        System.out.println(result);
+	}
 }
