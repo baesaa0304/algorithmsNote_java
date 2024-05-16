2435e6d3fb2a3f9201cab4bd528b5f5cf0223406
baesaa0304 <baesaa0304@naver.com>
2024-05-16 16:32:04 +0900

    DFS 풀이

---
+++ algorithms_Java02/src/algorithms_Java02_4/baekjoon_DFS_11724.java
@@ -0,0 +1,46 @@
+package algorithms_Java02_4;
+
+import java.util.*;
+import java.io.*;
+
+
+public class baekjoon_DFS_11724 {
+	static int N , M;
+	static int u , v;
+	static int[][] arr;
+	static boolean []visit;
+	static int cnt;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N+1][N+1];
+		visit = new boolean[N+1];
+		for(int i = 0; i < M; i++) {
+			StringTokenizer sts = new StringTokenizer(br.readLine());
+			u = Integer.parseInt(sts.nextToken());
+			v = Integer.parseInt(sts.nextToken());
+			arr[u][v] = arr[v][u] = 1;
+		}
+		for(int i =1; i <= N; i++) {
+			if(!visit[i]) {
+				dfs(i);
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+		
+	}
+	public static void dfs(int start) {
+		visit[start] = true;
+		for(int i = 1; i <= N; i++) {
+			if(arr[start][i] == 1 && !visit[i]) {
+				dfs(i);
+			}
+		}
+		
+	}
+
+}
