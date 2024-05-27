4f270945a9af3d5b77dd9fe8a5b8813e6b52372d
baesaa0304 <baesaa0304@naver.com>
2024-05-27 16:36:34 +0900

    백준 문제 BOJ 거리 커밋

--- algorithms_Java04/src/algorithms_Java04_01/baekjoon_12026.java
+++ algorithms_Java04/src/algorithms_Java04_01/baekjoon_12026.java
@@ -1,10 +1,33 @@
 package algorithms_Java04_01;
 
+import java.io.*;
+import java.util.*;
+
 public class baekjoon_12026 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		char[] street = br.readLine().toCharArray();
+		int [] dp = new int[N];
+		Arrays.fill(dp, Integer.MAX_VALUE);
+		dp[0] = 0;
+		for(int i = 1; i < dp.length; i++) {
+			for (int j = 0; j < i; j++) {
+				if (street[i] == 'O' && street[j] == 'B' && dp[j] != Integer.MAX_VALUE) {
+					dp[i] = Math.min(dp[i], dp[j] + (i-j)*(i-j));
+				}
+				else if(street[i] == 'J' && street[j] == 'O' && dp[j] != Integer.MAX_VALUE){
+					dp[i] = Math.min(dp[i], dp[j] + (i-j)*(i-j));
+				}else if(street[i] == 'B' && street[j] == 'J' && dp[j] != Integer.MAX_VALUE) {
+					dp[i] = Math.min(dp[i], dp[j] + (i-j)*(i-j));
+				}
+			}
+		}if (dp[N - 1] == Integer.MAX_VALUE) {
+            System.out.println(-1);
+        } else {
+            System.out.println(dp[N - 1]);
+        }
 	}
 
 }
