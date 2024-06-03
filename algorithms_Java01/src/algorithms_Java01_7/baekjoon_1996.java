2e98fe0dbcfdf31128112f3f3835588fdaf2a2bf
baesaa0304 <baesaa0304@naver.com>
2024-06-03 18:59:29 +0900

    1966번 문제 커밋 6월 3

---
+++ algorithms_Java01/src/algorithms_Java01_7/baekjoon_1996.java
@@ -0,0 +1,52 @@
+package algorithms_Java01_7;
+
+import java.util.*;
+import java.io.*;
+
+public class baekjoon_1996 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine()); // 테스트 케이스 
+ 
+		for(int i = 0; i < T; i++){
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int N = Integer.parseInt(st.nextToken()); // 문서의 개
+			int M = Integer.parseInt(st.nextToken()); // 원하는 문서의 위치 
+			
+			Queue<int[]> que = new LinkedList<>();	
+			st = new StringTokenizer(br.readLine());
+ 
+			for (int j = 0; j < N; j++) {
+				que.offer(new int[] { j, Integer.parseInt(st.nextToken()) }); // 각 문서들을 넣어줌 1, 2, 3, 4...
+			}
+ 
+			int count = 0;	// 출력 횟수
+			
+			while (!que.isEmpty()) {	// 한 케이스에 대한 반복문
+				int [] temp = que.poll();
+				boolean check = true;
+				// 큐를 순회하면서 중요도 비교
+				for(int []document : que) {
+					if(temp[1] < document[1]) {
+						que.offer(temp);
+						check = false;
+						break;
+					}
+				}
+				if(check) {
+					count ++;
+					if(temp[0] == M) {
+						sb.append(count).append('\n');
+						break;
+					}
+				}
+			}
+		}
+		System.out.println(sb);
+	}
+ 
+}
--- algorithms_Java04/src/algorithms_Java04_05/baekjoon_15989.java
+++ algorithms_Java04/src/algorithms_Java04_05/baekjoon_15989.java
@@ -1,8 +1,9 @@
 package algorithms_Java04_05;
 
 //필요한 패키지를 임포트합니다.
-import java.util.*;
+
 import java.io.*;
+import java.util.Arrays;
 
 public class baekjoon_15989 {
 
@@ -11,7 +12,7 @@ public class baekjoon_15989 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
 		StringBuilder sb = new StringBuilder();
-		int n = stoi(br.readLine());						
+		int n = Integer.parseInt(br.readLine());						
 		int[][] dp = new int[10001][4];// 각 숫자를 만드는 방법의 수를 저장
 		
 		// dp 배열의 초기값을 설정합니다.
@@ -28,21 +29,18 @@ public class baekjoon_15989 {
 			dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3]; 
 		}
 		for(int i = 0; i < n; i++) {
-			int t = stoi(br.readLine());
+			int t = Integer.parseInt(br.readLine());
 			sb.append(dp[t][1] + dp[t][2] + dp[t][3] + "\n");
 		}
 		
 		System.out.println(sb);
 		
-		// DP 배열 출력 코드 추가
-//        System.out.println("DP 배열 출력:");
-//        for(int i = 1; i <= 10; i++) { // 예시로 1부터 10까지만 출력
-//            System.out.println("Number " + i + ": " + Arrays.toString(dp[i]));
-//        }
+		 //DP 배열 출력 코드 추가
+        System.out.println("DP 배열 출력:");
+        for(int i = 1; i <= 10; i++) { // 예시로 1부터 10까지만 출력
+            System.out.println("Number " + i + ": " + Arrays.toString(dp[i]));
+        }
 	}
 	
-	public static int stoi(String str) {
-		return Integer.parseInt(str);
-	}
 }
 
