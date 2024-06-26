d0176ce137fdc945613fc7bb4340df388e634204
baesaa0304 <baesaa0304@naver.com>
2024-06-26 16:00:14 +0900

    문제 커밋

---
+++ algorithms03_DFS&&BFS/src/algorithms_Java02_15/baekjoon14226.java
@@ -0,0 +1,19 @@
+package algorithms_Java02_15;
+
+import java.util.*;
+import java.io.*;
+
+public class baekjoon14226 {
+	static boolean visit[][];
+	static int S;
+	static int emoticon_num;
+	static int clipboard_num;
+	static int time;
+	
+	
+	public static void main(String[] args) throws IOException{
+		
+
+	}
+
+}
