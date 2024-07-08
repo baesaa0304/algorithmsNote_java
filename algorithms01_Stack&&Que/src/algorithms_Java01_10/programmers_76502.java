b3bad8db8bcdce01481ee7595813eb0abb192b45
baesaa0304 <baesaa0304@naver.com>
2024-07-08 15:33:12 +0900

    7/8 문제 커밋

--- algorithms01_Stack&&Que/src/algorithms_Java01_10/programmers_76502.java
+++ algorithms01_Stack&&Que/src/algorithms_Java01_10/programmers_76502.java
@@ -5,52 +5,46 @@ import java.util.*;
 class Solution {
     public static int solution(String s) {
         int answer = 0;
-        Queue<String> que = new LinkedList<>();
-        
-        for(int i = 0; i < s.length(); i++){
-            String str = s.substring(i,i+1);
-            que.add(str);
+        Queue<Character> queue = new LinkedList<>();
+
+        // 문자열을 큐에 넣기
+        for (char c : s.toCharArray()) {
+            queue.offer(c);
         }
-        
-        
-        for(int i = 0; i < s.length(); i++){
-            String first = que.poll();
-            que.add(first);
-            
-            Stack<String> stack = new Stack<>();
-            
-            // stack에 전부 넣기
-            for(int j = 0; j < s.length(); j++){
-                String c = que.poll();
-                que.add(c);
-                
-                if(stack.isEmpty()){
+
+        for (int i = 0; i < s.length(); i++) {
+            Stack<Character> stack = new Stack<>();
+
+            // 큐의 첫 번째 문자부터 처리
+            for (int j = 0; j < s.length(); j++) {
+                char c = queue.poll();
+                queue.offer(c);
+
+                if (stack.isEmpty()) {
                     stack.push(c);
-                } 
-                else if(c.equals(")") && stack.peek().equals("(")){
+                } else if (c == ')' && stack.peek() == '(') {
                     stack.pop();
-                }
-                else if(c.equals("]") && stack.peek().equals("[")){
+                } else if (c == ']' && stack.peek() == '[') {
                     stack.pop();
-                }
-                else if(c.equals("}") && stack.peek().equals("{")){
+                } else if (c == '}' && stack.peek() == '{') {
                     stack.pop();
-                }
-                else{
+                } else {
                     stack.push(c);
                 }
-                
             }
-            
-            if(stack.isEmpty()){
+
+            if (stack.isEmpty()) {
                 answer++;
             }
-            
+
+            // 큐의 첫 번째 문자를 뒤로 보내기
+            queue.offer(queue.poll());
         }
-        
+
         return answer;
     }
 }
+
 
 public class programmers_76502 {
 	public static void main(String[] args) {
