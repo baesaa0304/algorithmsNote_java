92d94e497b9dab90e9ecb44c32d6ee7c9ae29db2
baesaa0304 <baesaa0304@naver.com>
2024-07-10 15:05:05 +0900

    커밋

--- algorithms01_Stack&&Que/src/algorithms_Java01_12/programmers_42584.java
+++ algorithms01_Stack&&Que/src/algorithms_Java01_12/programmers_42584.java
@@ -6,36 +6,33 @@ class Solution {
     public int[] solution(int[] prices) {
         int N = prices.length;
         int[] answer = new int[N];
-        /**
-         * 2중 For문 이용 
-         * 시간복잡도 : O(N^2)
-         * 스택이 가장 좋다.
-         */
-        for (int i = 0; i < N; i++) { 
-            int time = 0;
-            for (int j = i + 1; j < N; j++) {
-                time++;
-                if (prices[i] > prices[j]) { // 현재 주식 가격이 다음 주식 가격보다 크면 break
-                    break;
-                }
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+        	// 현 주식 가격이 다음 주식 가격 보다 큰 경우 즉 주식이 떨어진 경우 
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) { 
+                int idx = stack.pop();
+                answer[idx] = i - idx;
             }
-            answer[i] = time; // 각 지속된 시간을 answer에 저장 
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = N - idx - 1;
         }
 
         return answer;
     }
 }
 
-
 public class programmers_42584 {
-
-	public static void main(String[] args) {
-		Solution solution = new Solution();
-		int won[] = {1, 2, 3, 2, 3};
-		int result[] = solution.solution(won);
-		for(int m : result) {
-			System.out.println(m);
-		}
-	}
-
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int won[] = {1, 2, 3, 2, 3};
+        int result[] = solution.solution(won);
+        for (int m : result) {
+            System.out.println(m);
+        }
+    }
 }
