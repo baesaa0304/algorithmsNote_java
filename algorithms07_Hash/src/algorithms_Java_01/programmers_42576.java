8eea10718ba2ba2dc37972d9a710f3ce15538029
baesaa0304 <baesaa0304@naver.com>
2024-07-22 14:22:14 +0900

    커밋

--- algorithms07_Hash/src/algorithms_Java_01/programmers_42576.java
+++ algorithms07_Hash/src/algorithms_Java_01/programmers_42576.java
@@ -27,12 +27,12 @@ class Solution {
         for (String p : participant) {
             map.put(p, map.getOrDefault(p, 0) + 1); // p에 키 값이 있는 경우 0+1 아닌 경우 1++; 
         }
-        
+        System.out.println("해시 값 넣은 "+ map);
         // 완료자의 수를 감소
         for (String c : completion){
             map.put(c, map.get(c) - 1);
         }
-        
+        System.out.println("값 찾은" + map);
         // 수가 0이 아닌 참가자를 찾기
         for (String key : map.keySet()) {
             if (map.get(key) != 0) {
