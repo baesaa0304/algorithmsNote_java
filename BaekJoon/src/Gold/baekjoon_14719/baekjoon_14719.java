c4de4858a7aef6780020ad21cc04097e2fe31724
baesaa0304 <baesaa0304@naver.com>
2024-10-08 09:33:35 +0900

    10월 8일 문제 커밋

---
+++ BaekJoon/src/Gold/baekjoon_14719/baekjoon_14719.java
@@ -0,0 +1,38 @@
+package Gold.baekjoon_14719;
+
+import java.io.*;
+import java.util.*;
+
+public class baekjoon_14719 {
+
+	public static void main(String[] args) throws IOException {
+
+        BufferedReader
+        br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int H = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+
+        int[] blocks = new int[W];
+        int[] rain = new int[W];
+        st = new StringTokenizer(br.readLine());
+
+        int height = 0;
+        for (int i = 0; i < W; i++) {
+            blocks[i] = Integer.parseInt(st.nextToken());
+            height = Math.max(height, blocks[i]);
+            rain[i] = height;
+        }
+        height = 0;
+        int answer = 0;
+        for (int i = W - 1; i >= 0; i--) {
+            height = Math.max(height, blocks[i]);
+            rain[i] = Math.min(height, rain[i]);
+            answer += rain[i] - blocks[i];
+        }
+        System.out.println(answer);
+        br.close();
+    }
+}
+
